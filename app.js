591ade44e041863b007c16966f6cdfddeca5a77d
1712844 <38766087+1712844@users.noreply.github.com>
2021-05-30 05:14:08 +0100

    matched products

--- app.js
+++ app.js
@@ -3,26 +3,23 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var handlebarHelper = require('./helpers/handlebar.helper')
+var handlebarHelper = require('./helpers/handlebar.helper');
 require('dotenv').config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var productsRouter = require('./routes/products');
-var compareProductRouter = require('./routes/compareProduct');
 
 var app = express();
 
 // view engine setup
 var exphbs = require('express-handlebars');
 const hbs = exphbs.create({
-    defaultLayout: 'layout',
-    extname: 'hbs',
-    partialsDir: [
-        path.join(__dirname, 'views/partials'),
-    ],
-    helpers: require('./helpers/handlebar.helper').helpers
-})
+  defaultLayout: 'layout',
+  extname: 'hbs',
+  partialsDir: [path.join(__dirname, 'views/partials')],
+  helpers: require('./helpers/handlebar.helper').helpers,
+});
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
@@ -36,22 +33,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/products', productsRouter);
-app.use('/compareproduct', compareProductRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    next(createError(404));
+app.use(function (req, res, next) {
+  next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 module.exports = app;
