670ba8c742793dd43875c0754ae3c5788a021b3d
greent99 <55232248+greent99@users.noreply.github.com>
2021-05-30 14:04:30 +0700

    update

--- routes/products.js
+++ routes/products.js
@@ -23,7 +23,7 @@ router.get('/', async function(req, res, next) {
 
 router.get('/search', async function(req, res) {
     const page = req.query.page || 1;
-    const pageSize = req.query.pageSize || 12;
+    const pageSize = req.query.pageSize || 40;
     const keySearch = req.query.keySearch;
     const lengthItems = await itemModel.getTotalBySearch(keySearch);
     const totalPage = Math.ceil(lengthItems / pageSize);
