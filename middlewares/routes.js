2b205a80e5bfeb1fe81edb18966c5cc12e35c1e1
greent99 <55232248+greent99@users.noreply.github.com>
2021-06-03 00:10:21 +0700

    add layout view

--- middlewares/routes.js
+++ middlewares/routes.js
@@ -1,9 +1,11 @@
 var indexRouter = require('../routes/index');
 var usersRouter = require('../routes/users');
 var productsRouter = require('../routes/products');
+var authRouter = require('../routes/auth');
 
 module.exports = function(app) {
     app.use('/', indexRouter);
     app.use('/users', usersRouter);
     app.use('/products', productsRouter);
+    app.use('/auth', authRouter)
 }
