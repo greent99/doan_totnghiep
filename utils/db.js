00d5c4e0e3d8bdd5c8f707c2538709e92be1d8a9
greent99 <55232248+greent99@users.noreply.github.com>
2021-07-02 15:13:10 +0700

    update

--- utils/db.js
+++ utils/db.js
@@ -5,7 +5,8 @@ const knex = require('knex')({
       user: process.env.DB_USER,
       password: process.env.DB_PASS,
       database: process.env.DB_NAME,
-      port: + process.env.DB_PORT
+      port: + process.env.DB_PORT,
+      encrypt: process.env.DB_ENCRYPT === 'true' || true
     },
     pool: { min: 0, max: 50 }
   });
