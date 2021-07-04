46038807ae4f3b1f136183b4a0249a73065a0882
tvcong0999 <tvcong0999@gmail.com>
2021-07-04 23:24:59 +0700

    fix show promotion

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,17 @@ var express = require('express');
 const userModel = require('../models/user.model');
 const categoryModel = require('../models/category.model')
 var router = express.Router();
+const capitalize  = require('capitalize');
 
 /* GET home page. */
 router.get('/',async function(req, res, next) {
   let list_recommend_category = []
   let list_recommend_shop = []
-  const categorys = await categoryModel.getByLevel(1)
+  const categories = await categoryModel.getByLevel(1);
+  for(cate of categories)
+  {
+    cate.name = titleCase(cate.name.toUpperCase());
+  }
  
   if(req.session.isAuth)
   {
@@ -19,8 +24,19 @@ router.get('/',async function(req, res, next) {
     title: 'Express',
     list_recommend_category: list_recommend_category,
     list_recommend_shop: list_recommend_shop,
-    categorys: categorys
+    categories: categories
   });
 });
+
+function titleCase(str) {
+  var splitStr = str.toLowerCase().split(' ');
+  for (var i = 0; i < splitStr.length; i++) {
+      // You do not need to check if i is larger than splitStr length, as your for does that for you
+      // Assign it back to the array
+      splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);     
+  }
+  // Directly return the joined string
+  return splitStr.join(' '); 
+}
 
 module.exports = router;
